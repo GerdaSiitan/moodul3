0e908f5f6fa555c6df13605f07134f4f63780a23
gerdasiitan <gerda.siitan13@gmail.com>
2025-06-08 15:00:42 +0300

    Login page olemas, tooteid saab lisada koos vajalikute väljadega (pildid veel ei tööta, aga kõik muu olemas)

---
+++ app/Models/Product.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use CrudTrait;
+    /** @use HasFactory<\Database\Factories\ProductFactory> */
+    use HasFactory;
+
+    protected $fillable = ['name', 'price', 'description', 'color', 'size', 'image'];
+
+}
